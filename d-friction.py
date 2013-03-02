0272aaeaddd0d13114594876ea1680f4595b126b
Robert Markoski <robche@gmail.com>
2013-03-02 16:27:11 +0900

    added inputs. cleaned up code.

--- d-friction.py
+++ d-friction.py
@@ -6,26 +6,34 @@ The D'Arcy Friction Factor is used extensivley in fluid system design.
 Calculating its value can often be cumbersome, and prone to error when performed by hand.
 The following little program/function will calculate the D'Arcy Friction Factor given values for the Reynolds Number, Pipe Diameter and Pipe Roughness
 
-Now obviously you can use the several approximations of the implicit Colebrook-White equation such as the Swammee-Jain Equation (which I have also included a function for, it takes the same inputs as the Colebrook-White Equation.
-As for most applications 4 significant figures is all that is required, either equation may be used.
+Colebrook Equation - http://en.wikipedia.org/wiki/Darcy_friction_factor_formulae#Colebrook_equation
+Swamee Jain Equation -http://en.wikipedia.org/wiki/Darcy_friction_factor_formulae#Swamee.E2.80.93Jain_equation
 
-Colebrook Equation - http://en.wikipedia.org/wiki/.........
-D'Arcy Friction Factor - http.////
-Swamee Jain Equation
-
-
-You are obviously free to use this in any way/shape/form with or without my attribution.
-I am only human however and am not liable if anything goes wrong whilst you use this program. If you are a good engineer you would check and triple check all your values.
+You are free to use this in any way/shape/form with or without my attribution.
+I am not liable if anything goes wrong whilst you use this program.
 """
 import math # Import Handy Math Functions
 
-######## EDIT THESE VALUES #######
-flow = 25           # Flow in L/s
-diameter = 0.14636  # Pipe Diameter in m
-roughness = 0.007   # Pipe Roughness in mm
-density = 1020      # Density of Fluid in kg/m3
-viscosity = 0.001   # Visocsity of Fluid in Pa s
-##################################
+flow = raw_input("Please enter Flowrate in L/s: ")
+diameter = raw_input("Please enter Pipe Internal Diameter in mm: ")
+roughness = raw_input("please enter Material Roughness in mm [0.007]: ")
+density = raw_input("Please enter Fluid Density in kg/m3 [998]: ")
+viscosity = raw_input("Please enter Fluid Viscosity in Pa.s [0.001]: ")
+
+if not roughness:
+    roughness = 0.007 #Default HDPE
+if not density:
+    density = 998    #Default Ddensity of water @ 20C
+if not viscosity:     
+    viscosity = 0.001 #Default Viscosty of Water @ 20C
+
+#Turn all values into floating points
+flow = float(flow)
+diameter = float(diameter)/1000
+roughness = float(roughness)
+density = float(density)
+viscosity = float(viscosity)
+
 
 pipearea = diameter**2 / 4 * math.pi #Pipe Area in m^2
 
@@ -39,6 +47,9 @@ def CalculateF(diameter,roughness,reynolds):
         leftF = 1 / friction**0.5 #Solve Left side of Eqn
         rightF = - 2 * math.log10(2.51/(reynolds * friction**0.5)+(roughness/1000)/(3.72*diameter)) # Solve Right side of Eqn
         friction = friction - 0.000001 #Change Friction Factor
+      #  print(leftF)
+      #  print(rightF)
+      #  print(friction)
         if (rightF - leftF <= 0): #Check if Left = Right
             break
     return friction
@@ -47,6 +58,11 @@ def SwameeJain(diameter, roughness, reynolds):
     return 0.25 / (math.log10((roughness/1000)/(3.7*diameter)+5.74/(reynolds**0.9)))**2
 
 friction = CalculateF(diameter,roughness,reynolds)
-print("Reynolds Number is {:.0f}".format(reynolds))
-print("Darcy Friction Factor is {:.4f}".format(friction))
-print("Swamee-Jain - Darcy Friction Factor is {:.4f}".format(SwameeJain(diameter,roughness,reynolds)))
+
+#Print results in nice 'table'
+print("\n------------RESULTS-------------")
+print("Reynolds Number\t|\t{:.0f}".format(reynolds))
+print("Velocity (m/s)\t|\t{:.3f}".format(velocity))
+print("---------FRICTION FACTOR--------")
+print("Colebrook-White\t|\t{:.4f}".format(friction))
+print("Swamee-Jain\t|\t{:.4f}".format(SwameeJain(diameter,roughness,reynolds)))
